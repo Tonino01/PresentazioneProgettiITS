01875dd505ad5323cfead0f1d7959d68564e7028
Tonino01 <tonindanieltonin@gmail.com>
2026-03-11 14:32:25 +0100

    primo stampo app

---
+++ general.js
@@ -0,0 +1,22 @@
+function Tornio() {
+    window.location.href = "Mes-SimApp/index.html";
+}
+
+function Login() {
+    window.location.href = "Login/index.html";
+}
+
+function SitoITS() {
+    window.location.href = "RicreazioneSitoITS/index.html";
+}
+
+function TabellaCorsi() {
+    window.location.href = "TabellaCorsi/index.html";
+}
+function Tris() {
+    window.location.href = "Tris/index.html";
+}
+
+function Presentazione() {
+    window.location.href = "Presentazione/index.html";
+}
